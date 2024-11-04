ab76340352adb3b5ec4cd8b2d909895d86e80125
riskayuu <2300016028@webmail.uad.ac.id>
2024-11-04 23:40:18 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    const mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.classList.toggle('hidden');
+  });
